d8dd3e61c9327aac6328c54c861a1a9b1c98bdb3
Addwin Alanolikkal <147745866+add-win@users.noreply.github.com>
2026-09-20 15:27:32 +0530

    Update app for Loan Approval Prediction model

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import joblib
+import pandas as pd
+
+model = joblib.load("loan_approval_decision_tree.pkl")
+
+st.title("Loan Approval Prediction")
+
+income = st.number_input(
+    "Enter the Income",
+    min_value=1.0,
+    value=1.0,
+    step=1.0
+)
+
+credit = st.number_input(
+    "Enter the Credit Score",
+    min_value=1.0,
+    value=1.0,
+    step=1.0
+)
+
+if st.button("Predict"):
+
+    input_data = pd.DataFrame({
+        "Income": [income],
+        "Attendance": [credit]
+    })
+
+    prediction = model.predict(input_data)
+
+    if prediction[0] == 1:
+        st.success("🎉 Loan Approved!")
+    else:
+        st.error("❌ Loan not Approved!")
